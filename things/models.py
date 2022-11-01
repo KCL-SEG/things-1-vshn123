417be9eef05b97c818fb2bd63cc9a07089a55feb
Shaan Vieru <hshaan@ymail.com>
2022-11-01 23:47:37 +0000

    fix

--- things/models.py
+++ things/models.py
@@ -5,6 +5,6 @@ from django.core.validators import MaxValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=30, unique=True)#, validators=[RegexValidator(regex=r'^.+$', message='String can\'t be blank')])
-    description = models.CharField(max_length=120, unique=False)#, unique=False)
-    quantity = models.IntegerField()#validators=[MinValueValidator(limit_value=0), MaxValueValidator(limit_value=100)])
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, unique=False, blank=True)
+    quantity = models.IntegerField(validators=[MinValueValidator(limit_value=0), MaxValueValidator(limit_value=100)])
