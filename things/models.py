e3f215c6d3c8fb2723f21457317d6216235cb8d4
Shaan Vieru <hshaan@ymail.com>
2022-11-01 23:05:13 +0000

    Fix constraints

--- things/models.py
+++ things/models.py
@@ -5,6 +5,6 @@ from django.core.validators import MaxValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=30, unique=True, validator=[RegexValidator(regex=r'^.+$')])
+    name = models.CharField(max_length=30, unique=True, validators=[RegexValidator(regex=r'^.+$')])
     description = models.TextField(max_length=120)
-    quantity = models.IntegerField(validator=[MinValueValidator(limit_value=0), MaxValueValidator(limit_value=100)])
+    quantity = models.IntegerField(validators=[MinValueValidator(limit_value=0), MaxValueValidator(limit_value=100)])
