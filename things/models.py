a5f86644f1250737e386eccaf4c0f7a49418b9c8
Shaan Vieru <hshaan@ymail.com>
2022-11-01 23:01:08 +0000

    Add constraints

--- things/models.py
+++ things/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True, validators=[RegexValidator(r'^.+$')])
+    description = models.TextField(max_length=120)
+    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
